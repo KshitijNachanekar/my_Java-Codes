50814e1115dca975d2df635d04cef2ee71fe7484
Kshitij Nachanekar <101416006+Iamgamelord@users.noreply.github.com>
2025-06-09 12:16:25 +0530

    Create StarpyramidDemo.java

---
+++ StarpyramidDemo.java
@@ -0,0 +1,11 @@
+public class StarpyramidDemo {
+    public static void main(String[]args){
+        for(int i=0;i<5;i++){
+            for(int s=0,s)
+            for(int j=5;j>i;j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
