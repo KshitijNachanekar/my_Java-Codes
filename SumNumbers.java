01938fd66cc81b5b4eeb6ddc0563fdfc303fcb26
Kshitij Nachanekar <101416006+Iamgamelord@users.noreply.github.com>
2025-06-09 12:16:30 +0530

    Create SumNumbers.java

---
+++ SumNumbers.java
@@ -0,0 +1,9 @@
+public class SumNumbers {
+    public static void main(String[] args) {
+        int a = 0;
+        for (int i = 1; i <= 100; i++) {
+            a += i;
+        }
+        System.out.println("The sum of numbers from 1 to 100 is: " + a);
+    }
+}
