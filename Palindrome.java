ab8adf68e634cd07e23e77b82afbe0af87e39344
Kshitij Nachanekar <101416006+Iamgamelord@users.noreply.github.com>
2025-06-10 11:27:20 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        String reversed = "";
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i);
+        }
+
+        if (input.equalsIgnoreCase(reversed)) {
+            System.out.println("It is a palindrome.");
+        } else {
+            System.out.println("It is not a palindrome.");
+        }
+
+        scanner.close();
+    }
+}
